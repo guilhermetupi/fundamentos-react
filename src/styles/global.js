83ea9174356a3fa942d7ada8f9c9dfb531144d1e
Guilherme Tupinambá Durães <40327476+guilhermetupi@users.noreply.github.com>
2021-11-08 00:49:51 -0300

    Applying theming

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,9 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export default createGlobalStyle`
   body {
     background: #222;
-    color: #eee;
+    color: #fff;
     font-family: sans-serif;
-    margin: 0;
-    padding: 0;
   }
 `;
